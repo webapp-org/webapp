9de9076472594accd713327e272963e407a519dc
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-02-12 18:43:34 -0500

    Moved database logic to seperate file, added babel and supertest

--- dbConfig/index.js
+++ dbConfig/index.js
@@ -1,6 +1,5 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
-import mysql from "mysql2/promise";
 
 // dotenv package installed to store database variables
 dotenv.config();
@@ -14,53 +13,7 @@ const sequelize = new Sequelize(
     dialect: "mysql",
   }
 );
-async function createDatabaseIfNotExists() {
-  try {
-    // Create a MySQL connection
-    const connection = await mysql.createPool({
-      host: process.env.HOST,
-      user: process.env.USERNAME,
-      password: process.env.PASSWORD,
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query(
-      `CREATE DATABASE IF NOT EXISTS \`${process.env.DATABASE}\`;`
-    );
-
-    // Close the connection
-    await connection.end();
-  } catch (error) {
-    console.error("Error creating database:", error);
-  }
-}
-
-async function authenticateDatabase() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection to the database successful.");
-    return true;
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    throw new Error("Error in authenticate database");
-  }
-}
-
-async function syncModels(models = []) {
-  try {
-    for (const model of models) {
-      await model.sync({ alter: true });
-    }
-    return true;
-  } catch (error) {
-    console.error("Error synchronizing models:", error);
-    return false;
-  }
-}
 
 export default {
   sequelize,
-  createDatabaseIfNotExists,
-  authenticateDatabase,
-  syncModels,
 };
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import registerRouter from "./routes/index.js";
 import morgan from "morgan";
 import db from "./dbConfig/index.js";
 import User from "./models/User.js";
+import { initializeDatabase } from "./dbSetup/dbSetup.js";
 
 const app = express();
 
@@ -13,26 +14,17 @@ app.use(morgan("dev"));
 //Initialize routes
 registerRouter(app);
 
-db.createDatabaseIfNotExists().then(async (res) => {
-  try {
-    const databaseAuthenticated = await db.authenticateDatabase();
-    if (databaseAuthenticated) {
-      const models = [User];
-      // sync db
-      const modelsSynced = await db.syncModels(models);
-      if (modelsSynced) {
-        // start the server
-        const port = process.env.PORT || 4000;
-        app.listen(port, () => {
-          console.log(`Server running successfully on port ${port}`);
-        });
-      } else {
-        console.error("Model synchronization failed.");
-      }
-    } else {
-      console.error("Database authentication failed.");
+initializeDatabase()
+  .then((result) => {
+    if (result) {
+      const port = process.env.PORT || 4000;
+      app.listen(port, () => {
+        console.log(`Server running successfully on port ${port}`);
+      });
     }
-  } catch (error) {
-    console.log(error);
-  }
-});
+  })
+  .catch((error) => {
+    console.error("Error initializing server:", error);
+  });
+
+export default app;
