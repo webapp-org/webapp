5c519cacf60cfe188c37cae7512033fc37ddd206
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-02-21 18:55:28 -0500

    Updated packer

--- packer/scripts/move_app.sh
+++ packer/scripts/move_app.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
+# Ad new user
 sudo adduser csye6225 --shell /usr/sbin/nologin
 
+# Move webapp from tmp to opt
 sudo mv /tmp/webapp.zip /opt/webapp.zip
+
 # set ownership
 sudo chown csye6225:csye6225 /opt/webapp.zip
 sudo unzip /opt/webapp.zip -d /opt/webapp
