251a77d216e8f5e9ccd00999ea2cfb78b2319709
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-02-25 19:05:21 -0500

    Fixed packer issue

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
-# Install MySQL
-sudo yum install mysql-server -y
-
-# Start MySQL server
-sudo systemctl start mysqld.service
-
-# Update MySQL password
-mysql --connect-expired-password -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
 # Install Node.js
 curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
@@ -15,6 +7,5 @@ sudo yum install -y nodejs
 
 # Install unzip and unzip the assignment folder
 sudo yum install -y zip unzip
-# unzip Chinmay_Gulhane_002831871_02-1.zip 
 
 echo "Dependencies installed successfully."
