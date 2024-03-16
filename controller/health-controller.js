25eb921682d09961de048ff251d1381a9f1bd99c
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-03-16 16:30:08 -0400

    Added node-json-logger

--- controller/health-controller.js
+++ controller/health-controller.js
@@ -1,9 +1,12 @@
 import db from "../dbConfig/index.js";
+import Logger from "node-json-logger";
 
 export const healthCheck = async (req, res) => {
   try {
     await db.sequelize.authenticate();
     res.json();
+    const logger = new Logger();
+    logger.info("Database connection successfull");
   } catch (error) {
     res.status(503).json();
   }
