4e1ff7c312924f21e6b1d2eb1d7e3108c70f4a2b
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-03-17 14:03:25 -0400

    Updated logger config and moved google ops config to a file

--- packer/scripts/google_ops_agent.sh
+++ packer/scripts/google_ops_agent.sh
@@ -6,29 +6,5 @@ set -e
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-# Add ops agent configuration
-sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null <<'EOL'
-logging:
-  receivers:
-    custom_log:
-      type: files
-      include_paths:
-      - /var/log/webapp/webapp.log
-  processors:
-    parse_json_logs:
-      type: parse_json
-    set_severity:
-      type: modify_fields
-      fields:
-        severity:
-          copy_from: jsonPayload.level
-          map_values:
-            "info": "INFO"
-            "error": "ERROR"
-            "warn": "WARNING"
-  service:
-    pipelines:
-      default_pipeline:
-        receivers: [custom_log]
-        processors: [parse_json_logs, set_severity]
-EOL
+# Move ops agent config
+sudo mv ./config.yaml /etc/google-cloud-ops-agent/config.yaml
