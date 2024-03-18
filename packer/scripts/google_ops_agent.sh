1bf160d2b14f5704ff421d9328bb8e88dc1a7ab0
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-03-18 17:11:22 -0400

    google ops agent change

--- packer/scripts/google_ops_agent.sh
+++ packer/scripts/google_ops_agent.sh
@@ -8,3 +8,4 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Move ops agent config
 sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent
+sudo systemctl restart google-cloud-ops-agent
