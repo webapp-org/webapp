9141f307e3cf646ce3f1722f75c2483982a435bd
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-03-18 15:15:26 -0400

    Updated logger config

--- logger/logger.js
+++ logger/logger.js
@@ -1,25 +1,16 @@
 import { createLogger, format, transports } from "winston";
-import moment from "moment-timezone";
-
-// Custom time zone format
-const timezoneFormat = format((info) => {
-  // Local Time
-  info.timestamp = moment()
-    .tz("America/New_York")
-    .format("YYYY-MM-DD HH:mm:ss");
-  // UTC
-  // info.timestamp = moment().utc().format("YYYY-MM-DD HH:mm:ss");
-  return info;
-})();
 
 const logger = createLogger({
-  format: format.combine(timezoneFormat, format.json()),
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    format.json()
+  ),
   transports: [
     new transports.File({
       filename:
-        process.env.ENV === "dev"
-          ? "./webapp.log"
-          : "/var/log/webapp/webapp.log",
+        process.env.ENV === "dev" ? "webapp.log" : "/var/log/webapp/webapp.log",
     }),
   ],
 });
