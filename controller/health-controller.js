3f2fd553a5904005572f5f9f20471fb08a4c5ad0
Chinmay Gulhane <gulhane.c@northeastern.edu>
2024-03-19 23:30:00 -0400

    Updated logs

--- controller/health-controller.js
+++ controller/health-controller.js
@@ -5,27 +5,58 @@ export const healthCheck = async (req, res) => {
   try {
     await db.sequelize.authenticate();
     res.json();
+    // success log
     logger.info({
       message: "Health check successful",
       action: "Health Check API",
       status: "success",
+      httpRequest: {
+        method: req.method,
+        url: req.url,
+        userAgent: req.headers["user-agent"],
+        status: res.statusCode,
+      },
     });
+
+    // sample warning log
     logger.warn({
       message: "Sample warning log",
       action: "Health Check API",
       status: "success",
+      httpRequest: {
+        method: req.method,
+        url: req.url,
+        userAgent: req.headers["user-agent"],
+        status: res.statusCode,
+      },
     });
+
+    // sample debug log
     logger.debug({
       message: "Sample debug log",
       action: "Health Check API",
       status: "success",
+      httpRequest: {
+        method: req.method,
+        url: req.url,
+        userAgent: req.headers["user-agent"],
+        status: res.statusCode,
+      },
     });
   } catch (error) {
+    // error log
     logger.error({
       message: "Database connection failed during health check",
       action: "Health Check API attempt",
       status: "failed",
-      error: error,
+      error: error.message,
+      stack: error.stack,
+      httpRequest: {
+        method: req.method,
+        url: req.url,
+        userAgent: req.headers["user-agent"],
+        status: 503,
+      },
     });
     res.status(503).json();
   }
--- logger/logger.js
+++ logger/logger.js
@@ -1,12 +1,7 @@
 import { createLogger, format, transports } from "winston";
 
 const logger = createLogger({
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    format.json()
-  ),
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.File({
       level: "debug",
